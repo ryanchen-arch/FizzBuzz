fa210b3d98808903caf6f39a7ab8266482cd50da
Ryan Chen <ryanchen604@gmail.com>
2023-09-11 10:16:12 -0400

    finished Reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int output = 0;
+        int n = 100;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            }
+            else {
+                n -= 1;
+            }
+            output++;
+        }
+        System.out.println(output);
+    }
 }
